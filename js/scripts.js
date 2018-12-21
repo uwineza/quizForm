04ce56953d8755477e3426120a6073129a540bb4
UWINEZA FLORENTINE <niyinezafofo@gmail.com>
2018-12-21 14:28:59 +0200

    add function of checkedvalue

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 var answers = ["D", "A", "C", "A", "C", "B", "D", "B", "B", "A"],
     tot = answers.length;
+
+function getCheckedValue(radioName) {
+    var radios = document.getElementsByName(radioName); // Get radio group by-name
+    for (var y = 0; y < radios.length; y++)
+        if (radios[y].checked)
+            return radios[y].value;
+    // return the checked value
+
+}
